530b485a7973139cad4085c09b3a313eb679cd95
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2022-08-04 14:06:11 +0300

    build: configure Tailwind CSS

---
+++ apps/nextjs-app/postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {
+      config: 'apps/nextjs-app/tailwind.config.js',
+    },
+    autoprefixer: {},
+  },
+};
---
+++ apps/nextjs-app/tailwind.config.js
@@ -0,0 +1,4 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['apps/nextjs-app/src/**/*.{js,ts,jsx,tsx}'],
+};
