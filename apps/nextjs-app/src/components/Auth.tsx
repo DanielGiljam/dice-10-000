d8292fe6960a53b51e528d90c7b31a24bbd3d82c
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2022-08-04 13:05:38 +0300

    feat: customize FirebaseUI Auth

--- apps/nextjs-app/src/components/Auth.tsx
+++ apps/nextjs-app/src/components/Auth.tsx
@@ -6,14 +6,15 @@ import 'firebase/compat/auth';
 
 const uiConfig: StyledFirebaseAuth['props']['uiConfig'] = {
   signInOptions: [
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-    firebase.auth.GithubAuthProvider.PROVIDER_ID,
-    firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-    'anonymous',
+    {
+      provider: 'google.com',
+      clientId:
+        '1031537215522-a5p950j8f4l323bn5t4n99p5i393hrp2.apps.googleusercontent.com',
+    },
+    'apple.com',
+    'password',
   ],
+  credentialHelper: 'googleyolo',
 };
 
 export const Auth: React.FC<{ firebaseAuth: firebase.auth.Auth }> = ({
