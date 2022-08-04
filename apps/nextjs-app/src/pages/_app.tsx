62f4bea42a1c654716458a8321e87d75a3c4af1a
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2022-08-04 14:06:47 +0300

    feat: add title tag

--- apps/nextjs-app/src/pages/_app.tsx
+++ apps/nextjs-app/src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import './styles.css';
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <>
+    <Head>
+      <title>Dice 10 000</title>
+    </Head>
+    <Component {...pageProps} />
+  </>
 );
 
 export default App;
