43c5c1041e987fe24ec0cd5b48f32312b2d77c15
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2022-09-10 14:12:48 +0300

    chore: run nx generate @nrwl/node:application
    --name=create-test-data
    --setParserOptionsProject

---
+++ apps/create-test-data/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'create-test-data',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/create-test-data',
+};
